599eb594d2cc6957bd28251b0f99798476c4e5e8
Sohrab Namazinia <sohrab.namazinia@gmail.com>
2023-07-18 15:24:09 -0400

    finish environment and combined policy

--- env/gridworld.py
+++ env/gridworld.py
@@ -4,12 +4,13 @@ import numpy as np
 
 class GridWorld(gym.Env):
     
-    def __init__(self, grid_size, reward_system, agent_position, target_position, cell_low_value, cell_high_value, 
+    def __init__(self, grid_width, grid_length, reward_system, agent_position, target_position, cell_low_value, cell_high_value, 
         start_position_value, target_position_value, gold_positions=None, block_positions=None):
         
         super(GridWorld, self).__init__()
 
-        self.grid_size = grid_size
+        self.grid_width = grid_width
+        self.grid_length = grid_length
         self.agent_position = agent_position # e.g., [0, 0]
         self.start_position = agent_position # e.g., [0, 0]
         self.target_position = target_position # e.g., [4, 4]
@@ -21,10 +22,10 @@ class GridWorld(gym.Env):
 
         self.action_space = spaces.Discrete(4)
         self.observation_space = spaces.Box(low = cell_low_value,
-            high= cell_high_value, shape=(grid_size, grid_size))
+            high= cell_high_value, shape=(self.grid_width, self.grid_length))
 
         # Initialize the grid
-        self.grid = np.zeros((grid_size, grid_size))
+        self.grid = np.zeros((self.grid_width, self.grid_length))
         self.grid[self.start_position[0]][self.start_position[1]] = start_position_value # e.g., 5
         self.grid[self.target_position[0]][self.target_position[1]] = target_position_value # e.g., 10
 
@@ -57,7 +58,7 @@ class GridWorld(gym.Env):
         elif action == 3: # left
             self.agent_position[1] -= 1
 
-        self.agent_position = np.clip(self.agent_position, 0, self.grid_size-1)
+        self.agent_position = np.clip(self.agent_position, (0, 0), (self.grid_width - 1, self.grid_length - 1))
 
         reward = self._get_reward(prev_agent_position)
         done = np.array_equal(self.agent_position, self.target_position)
@@ -69,41 +70,45 @@ class GridWorld(gym.Env):
         
         # gold collection task
         if self.reward_system == "gold":
-            reward = 0
-            candidates = []
-            current_cell_value = self.grid[self.agent_position[0]][self.agent_position[1]]
-            if current_cell_value == -1:  # block
-                return self.MINE_REWARD
-            for i in range(-2, 3):
-                for j in range(-2, 3):
-                    new_candidate = [self.agent_position[0] + i, self.agent_position[1] + j]
-                    new_candidate = np.clip(new_candidate, 0, self.grid_size-1).tolist()
-                    if new_candidate not in candidates:
-                        candidates.append(new_candidate)
-            for candidate in candidates:
-                cell_value = self.grid[candidate[0], candidate[1]]
-                if cell_value == 1:
-                    reward += 1
-            if current_cell_value == 1:  # gold
-                self.grid[self.agent_position[0]][self.agent_position[1]] = 0
-            return reward
+            return self.get_reward_gold()
 
         # shortest path task
         elif self.reward_system == "path":
-            current_cell_value = self.grid[self.agent_position[0]][self.agent_position[1]]
-            if current_cell_value == -1:  # block
-                return self.MINE_REWARD
-            d1 = np.sum(np.abs(np.array(prev_agent_position) - np.array(self.target_position)))
-            d2 = np.sum(np.abs(np.array(self.agent_position) - np.array(self.target_position)))
-            r = d1 - d2
-            return r
+            return self.get_reward_path(prev_agent_position)
+        
+        elif self.reward_system == "combined":
+            return self.get_reward_gold() + self.get_reward_path(prev_agent_position)
 
         return 0
 
     def render(self, mode='human'):
         print(self.grid)
 
-    def manhattan_dist(self, state1, state2):
-        term1 = abs(state1.x - state2.x)
-        term2 = abs(state1.y - state2.y)
-        return (term1 + term2)
+    def get_reward_path(self, prev_agent_position):
+        current_cell_value = self.grid[self.agent_position[0]][self.agent_position[1]]
+        if current_cell_value == -1:  # block
+            return self.MINE_REWARD
+        d1 = np.sum(np.abs(np.array(prev_agent_position) - np.array(self.target_position)))
+        d2 = np.sum(np.abs(np.array(self.agent_position) - np.array(self.target_position)))
+        r = d1 - d2
+        return r    
+        
+    def get_reward_gold(self):
+        reward = 0
+        candidates = []
+        current_cell_value = self.grid[self.agent_position[0]][self.agent_position[1]]
+        if current_cell_value == -1:  # block
+            return self.MINE_REWARD
+        for i in range(-2, 3):
+            for j in range(-2, 3):
+                new_candidate = [self.agent_position[0] + i, self.agent_position[1] + j]
+                new_candidate = np.clip(new_candidate, (0, 0), (self.grid_width - 1, self.grid_length - 1)).tolist()
+                if new_candidate not in candidates:
+                    candidates.append(new_candidate)
+        for candidate in candidates:
+            cell_value = self.grid[candidate[0], candidate[1]]
+            if cell_value == 1:
+                reward += 1
+        if current_cell_value == 1:  # gold
+            self.grid[self.agent_position[0]][self.agent_position[1]] = 0
+        return reward
--- train_deep_policy.py
+++ train_deep_policy.py
@@ -7,22 +7,23 @@ import numpy as np
 timesteps = 10000
 deep_algorithm = "DQN" # either "DQN" or "A2C"
 reward_system = "gold" # either "gold" or "path"
-gold_positions = [[2, 0], [2, 2], [5, 2], [1, 5]]
+gold_positions = [[0, 2], [2, 2], [2, 5], [4, 1]]
 block_positions = []
 agent_initial_position = [0,0]
-target_position = [5, 5]
+target_position = [4, 4]
 cell_low_value = -1
 cell_high_value = 10
 start_position_value = 5
 target_position_value = 10
 max_steps_per_episode = 100
-grid_size = 6
+grid_width = 5
+grid_length = 6
 
 # Initialize a new run
 run = wandb.init(project="gridworld", entity="sn773")
 
 # Instantiate environment
-grid_world = GridWorld(grid_size= grid_size, reward_system = reward_system, 
+grid_world = GridWorld(grid_width=grid_width, grid_length=grid_length , reward_system = reward_system, 
 	agent_position = agent_initial_position, target_position = target_position,
 	cell_low_value = cell_low_value, cell_high_value = cell_high_value,
 	start_position_value = start_position_value, target_position_value = target_position_value,
