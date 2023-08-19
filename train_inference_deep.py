50fa46cc5ec9fd9e8a9a66c3090db07fb90021a3
FertileJungle91 <one.giant.cowboy@gmail.com>
2023-08-19 13:15:11 -0400

    reverted train_deep_policy.py back to original state, and modified _on_step function to count the time

--- train_inference_deep.py
+++ train_inference_deep.py
@@ -1,6 +1,7 @@
 from train_deep_policy import train_deep_policy
 from env.init_gridworld import init_gridworld_1
 from Inference_deep import inference_deep
+from agents.deep_agent import Agent
 
 # Define env and train parameters
 reward_system = "combined"
@@ -10,10 +11,16 @@ deep_algorithm = "PPO"
 output_path = "agent_new.pkl"
 
 # train + inference
-total_train_time = train_deep_policy(timesteps, deep_algorithm, reward_system, output_path)
+
+deep_agent = Agent(grid_world, deep_algorithm)
+deep_agent.learn(timesteps)
+deep_agent.save(output_path)
+
+total_train_time = deep_agent.callback.total_time
+
 grid_world = init_gridworld_1(reward_system)
 deep_agent_path = output_path
-total_inference_time = inference_deep(grid_world, deep_algorithm, output_path)
+total_inference_time = inference_deep(grid_world, deep_algorithm, deep_agent_path)
 total_time = total_train_time + total_inference_time
 
 # output
